b0e132d71bd7a1d37cf68a2785136cc4a5c4229e
Kelly Robinson <kshimrob@gmail.com>
2017-05-25 10:05:19 -0500

    first commit

--- car_test.rb
+++ car_test.rb
@@ -5,26 +5,26 @@ Minitest::Reporters.use!
 require_relative 'car'
 
 class CarTest < MiniTest::Test
+  def setup
+    @car = Car.new
+  end
+
   def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+    assert_equal(4, @car.wheels)
   end
 
   def test_bad_wheels
     skip("Skipped test bad wheels")
-    car = Car.new
-    assert_equal(3, car.wheels)
+    assert_equal(3, @car.wheels)
   end
 
   def test_card_exists
-    car = Car.new
-    assert(car)
+    assert(@car)
   end
 
   def test_name_is_nil
     skip
-    car = Car.new
-    assert_nil(car.name)
+    assert_nil(@car.name)
   end
 
   def test_raise_initialize_with_arg
@@ -34,10 +34,17 @@ class CarTest < MiniTest::Test
   end
 
   def test_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
+    assert_instance_of(Car, @car)
     # better when dealing with inheritance
   end
+
+  def test_includes_car
+    arr = [1, 2, 3, @car]
+
+    assert_includes(arr, @car)
+  end
+
+  # use teardown method here if necessary to clean up files or logging info or close database connections
 end
 
 ## THIS IS KNOWN AS ASSERTION OR ASSERT-STYLE SYNTAX
